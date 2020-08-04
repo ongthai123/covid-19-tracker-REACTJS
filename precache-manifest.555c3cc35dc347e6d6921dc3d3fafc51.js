9c148b30bf4415bd5e1b5bdf6b97ef31b891ef10
ongthai <ongthai2803@gmail.com>
2020-08-04 16:08:00 +1200

    Updates

---
+++ precache-manifest.555c3cc35dc347e6d6921dc3d3fafc51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "763fc4e9f65a0287373afb95186221b6",
+    "url": "/covid-19-tracker-REACTJS/index.html"
+  },
+  {
+    "revision": "9b31ad28f46aaf5f9a7a",
+    "url": "/covid-19-tracker-REACTJS/static/css/main.25af4310.chunk.css"
+  },
+  {
+    "revision": "baeba76ede70cd2e7d5d",
+    "url": "/covid-19-tracker-REACTJS/static/js/2.820a3c12.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker-REACTJS/static/js/2.820a3c12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b31ad28f46aaf5f9a7a",
+    "url": "/covid-19-tracker-REACTJS/static/js/main.883cf009.chunk.js"
+  },
+  {
+    "revision": "06a1bff01a8b18b832b8",
+    "url": "/covid-19-tracker-REACTJS/static/js/runtime-main.ccc6c1ad.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker-REACTJS/static/media/image.d7265326.png"
+  }
+]);
